aac577a5f90d57acb68873bc897304532b88e9de
Robert Schulze <robert@clickhouse.com>
2022-07-13 14:55:29 +0000

    Don't auto-vectorize casting code

--- src/Functions/FunctionsConversion.h
+++ src/Functions/FunctionsConversion.h
@@ -192,6 +192,9 @@ struct ConvertImpl
             }
 
             bool result_is_bool = isBool(result_type);
+#ifdef __clang__
+#pragma clang loop vectorize(disable) // auto-vectorization leads to slower performance than if disabled
+#endif
             for (size_t i = 0; i < input_rows_count; ++i)
             {
                 if constexpr (std::is_same_v<ToDataType, DataTypeUInt8>)
